26abef180b4916e139a19309c23eefbb8d50239d
John-Thailand <naoki.honda@mirrorfit.jp>
2024-06-22 17:29:35 +0900

    ログイン画面の実装

--- resources/views/login/login_form.blade.php
+++ resources/views/login/login_form.blade.php
@@ -2,12 +2,28 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <!-- content="width=device-width"：Webページの幅をデバイスの幅に合わせる -->
+    <!-- initial-scale=1.0：ページの初期ズームレベルを1.0（つまり100%）に設定 -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ログインフォーム</title>
-    
 </head>
 <body>
-    
+    <!-- web.phpからルート名がloginのルートを参照する -->
+    <form method="POST" action="{{ route('login') }}">
+        <!-- CSRF攻撃から保護するために使用される -->
+        @csrf
+
+        <label for="email">メールアドレス</label>
+        <br>
+        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+
+        <br>
+        <label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
+        <br>
+        <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+        <br>
+        <button type="submit" class="btn btn-primary">ログイン</button>
+    </form>
 </body>
 </html>
