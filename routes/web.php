99bf08bf828e75e44d64985ef43d110ca4dd35ef
John-Thailand <naoki.honda@mirrorfit.jp>
2024-06-25 19:05:38 +0900

    ミドルウェアの設定

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,18 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthController;
 
-// ログインフォーム表示
-Route::get('/', [AuthController::class, 'showLogin'])->name('showLogin');
-// ログイン処理
-Route::post('login', [AuthController::class, 'login'])->name('login');
-// ホーム画面
-Route::get('home', function () {
-    return view('home');
-})->name('home');
+// ログイン前に実行できる処理のグループとして、'guest'と設定する
+Route::group(['middleware' => ['guest']], function () {
+    // ログインフォーム表示
+    Route::get('/', [AuthController::class, 'showLogin'])->name('showLogin');
+    // ログイン処理
+    Route::post('login', [AuthController::class, 'login'])->name('login');
+});
+
+// ログイン後に実行できる処理のグループとして、'auth'と設定する
+Route::group(['middleware' => ['auth']], function () {
+    // ホーム画面
+    Route::get('home', function () {
+        return view('home');
+    })->name('home');
+});
