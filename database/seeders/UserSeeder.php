1ecaa616e4f1b69ee9efb94b947c5deef0122434
John-Thailand <naoki.honda@mirrorfit.jp>
2024-06-25 18:46:26 +0900

    UserのSeederを暗号化してDBに保存

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -16,17 +17,17 @@ class UserSeeder extends Seeder
             [
                 'name' => 'なおき',
                 'email' => 'test@test.com',
-                'password' => 'test1234'
+                'password' => Hash::make('test1234')    // パスワードは暗号化してDBにデータを保存しておく
             ],
             [
                 'name' => 'おおたけ',
                 'email' => 'test1@test.com',
-                'password' => 'test1234'
+                'password' => Hash::make('test1234')
             ],
             [
                 'name' => 'よしお',
                 'email' => 'test2@test.com',
-                'password' => 'test1234'
+                'password' => Hash::make('test1234')
             ]
         ]);
     }
