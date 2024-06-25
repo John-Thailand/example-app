b513fe7a48859b60c0501254c82827489b7cf394
John-Thailand <naoki.honda@mirrorfit.jp>
2024-06-25 19:25:52 +0900

    テーブルの実装

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,11 +14,14 @@
                     {{ session('login_success') }}
                 </div>
             @endif
-            <h3>プロフィール</h3>
-            <ul>
-                <li>名前：{{ Auth::user()->name }}</li>
-                <li>メールアドレス：{{ Auth::user()->email }}</li>
-            </ul>
+            <table border="1">
+                <tr>
+                    <th>タイトル</th>
+                    <th>期限</th>
+                    <th>担当者</th>
+                    <th>内容</th>
+                </tr>
+            </table>
         </div>
     </div>
 </body>
