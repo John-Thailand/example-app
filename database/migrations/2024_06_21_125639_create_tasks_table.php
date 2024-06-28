fd00a0c11b08cfb84cd0984ccb62216a1e234851
John-Thailand <naoki.honda@mirrorfit.jp>
2024-06-28 20:28:00 +0900

    updated_atは必要

--- database/migrations/2024_06_21_125639_create_tasks_table.php
+++ database/migrations/2024_06_21_125639_create_tasks_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('title', 30);
             $table->dateTime('date');
             $table->text('description');
+            $table->timestamps();
         });
     }
 
