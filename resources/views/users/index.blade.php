4e49696b9fdb64cf234ab4c6fec9446b030e984d
John-Thailand <naoki.honda@mirrorfit.jp>
2024-07-03 20:12:22 +0900

    ユーザ一覧画面のタイトルを修正

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ホーム画面</title>
+    <title>ユーザ一覧画面</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="flex">
