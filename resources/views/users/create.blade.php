4596caff50810e56bda320e8a032e7efda851585
John-Thailand <naoki.honda@mirrorfit.jp>
2024-07-03 20:15:52 +0900

    パスワードを非表示にする

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -22,7 +22,7 @@
             </div>
             <div>
                 <label for="password" class="block text-sm font-medium text-gray-700">パスワード:</label>
-                <input type="text" id="password" name="password" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                <input type="password" id="password" name="password" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
             </div>
             <div>
                 <label for="password_confirmation" class="block text-sm font-medium text-gray-700">パスワードの確認:</label>
